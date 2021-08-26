3b526dc22ea8aeb163dcdcac251ae8bc17e646ff
JoA-MoS <joamos.dev@gmail.com>
2021-08-26 11:33:03 -0700

    style(plugins-docker): corrects prettier issues

--- packages/plugins/docker/src/lib/verify.ts
+++ packages/plugins/docker/src/lib/verify.ts
@@ -4,8 +4,8 @@ import { PluginConfig } from './plugin-config.interface';
 
 export async function verifyConditions(pluginConfig: PluginConfig, { logger }) {
   pluginConfig = normalizeConfig(pluginConfig);
-  if ( pluginConfig.skipLogin ) return;
-  
+  if (pluginConfig.skipLogin) return;
+
   for (const envVar of ['DOCKER_USERNAME', 'DOCKER_PASSWORD']) {
     if (!process.env[envVar]) {
       throw new Error(`Environment variable ${envVar} is not set`);
