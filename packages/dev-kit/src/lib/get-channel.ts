22f8feb0336de6a3d8533c33b393c4c6d8c24611
JoA-MoS <joamos.dev@gmail.com>
2021-10-05 09:36:49 -0700

    chore: remove devkit

--- packages/dev-kit/src/lib/get-channel.ts
+++
@@ -1,9 +0,0 @@
-import { validRange } from 'semver';
-
-export function getChannel(channel: string): string {
-  return channel
-    ? validRange(channel)
-      ? `release-${channel}`
-      : channel
-    : 'latest';
-}
