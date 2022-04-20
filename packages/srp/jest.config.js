e0111f64e1fccd09d2e839730b603d375987b685
JoA-MoS <JoA-MoS@users.noreply.github.com>
2022-04-19 20:52:27 -0700

    test: enables both jest and ava tests during transition to jest

--- packages/srp/jest.config.js
+++ packages/srp/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   displayName: 'srp',
   preset: '../../jest.preset.js',
+  // override default test matcher to allow jest test with .spec.ts files and ava with .test.js files
+  testMatch: ['**/+(*.)+(spec).+(ts|js)?(x)'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
