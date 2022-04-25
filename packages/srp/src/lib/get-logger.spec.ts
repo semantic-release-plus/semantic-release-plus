0cfcdd9d486fbb867613a1dffbd357880e16d3ff
JoA-MoS <JoA-MoS@users.noreply.github.com>
2022-04-25 08:47:31 -0700

    test(srp): switch get-logger to jest

--- packages/srp/src/lib/get-logger.spec.ts
+++ packages/srp/src/lib/get-logger.spec.ts
@@ -1,20 +1,20 @@
-const test = require('ava');
-const { spy } = require('sinon');
-const getLogger = require('../src/lib/get-logger');
+import * as getLogger from './get-logger';
 
-test('Expose "error", "success" and "log" functions', (t) => {
-  const stdout = spy();
-  const stderr = spy();
-  const logger = getLogger({
-    stdout: { write: stdout },
-    stderr: { write: stderr },
+describe('get-logger', () => {
+  test('Expose "error", "success" and "log" functions', () => {
+    const stdout = jest.fn();
+    const stderr = jest.fn();
+    const logger = getLogger({
+      stdout: { write: stdout },
+      stderr: { write: stderr },
+    });
+
+    logger.log('test log');
+    logger.success('test success');
+    logger.error('test error');
+
+    expect(stdout.mock.calls[0][0]).toMatch(/.*test log/);
+    expect(stdout.mock.calls[1][0]).toMatch(/.*test success/);
+    expect(stderr.mock.calls[0][0]).toMatch(/.*test error/);
   });
-
-  logger.log('test log');
-  logger.success('test success');
-  logger.error('test error');
-
-  t.regex(stdout.args[0][0], /.*test log/);
-  t.regex(stdout.args[1][0], /.*test success/);
-  t.regex(stderr.args[0][0], /.*test error/);
 });
