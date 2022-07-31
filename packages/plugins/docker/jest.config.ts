875bec80d81e0a580e232919c7187cb1097d135d
JoA-MoS <joamos.dev@gmail.com>
2022-07-31 07:34:04 -0700

    chore: updates nx to 14.5.1

--- packages/plugins/docker/jest.config.ts
+++ packages/plugins/docker/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   name: 'plugins-docker',
   preset: '../../../jest.preset.js',
   globals: {
--- packages/srp/jest.config.ts
+++ packages/srp/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'srp',
   preset: '../../jest.preset.js',
   // override default test matcher to allow jest test with .spec.ts files and ava with .test.js files
